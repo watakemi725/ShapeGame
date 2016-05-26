fe6572fbc82eb79a1090cbf598d94004ec369849
綿貫岳海 <takemi007@hotmail.com>
2016-05-26 17:55:44 +0900

    +++

--- ShapeGame/GameViewController.swift
+++ ShapeGame/GameViewController.swift
@@ -216,7 +216,6 @@ class GameViewController: UIViewController {
         }
     }
     
-    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
